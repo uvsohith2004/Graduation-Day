376ee5e1fb492c6c14898a5c9aadb17f6f9d73c3
Masani Yasovardhan <yasovardhanmasani@gmail.com>
2026-06-26 22:51:37 +0530

    fix: fixing the imports

--- apps/server/src/register/register.service.ts
+++ apps/server/src/register/register.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectDB } from '../database/inject-db.decorator';
-import { alumni } from 'src/database/schemas/register.schema';
-import { eligibility } from 'src/database/schemas/eligibility.schema';
+import { alumni,eligibility } from 'src/database/schemas';
 import { eq } from 'drizzle-orm';
 import { CreateAlumniDto } from './dto/create-alumni.dto';
 
