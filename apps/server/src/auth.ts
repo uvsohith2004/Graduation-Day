48c29296845b3e442f430019a91b8e08189001ae
Masani Yasovardhan <yasovardhanmasani@gmail.com>
2026-06-26 23:32:44 +0530

    chore:testing env variables

--- apps/server/src/auth.ts
+++ apps/server/src/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./database/db"; 
 import * as schema from "./database/schemas"; 
-
+import "dotenv/config"
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
